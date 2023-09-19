33bf91f427de4e3b02315a0e54d23c57ccd387b4
lespam <lesliepbv@gmail.com>
2023-09-19 19:49:24 -0400

    fixed checkbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,19 @@
 /* sweetScroll load */
 document.addEventListener("DOMContentLoaded", function () {
-  const sweetScroll = new SweetScroll({/* some options */});
+  const sweetScroll = new SweetScroll({/* some options */ });
 
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS.load("particles-js", "assets/particlesjs-config.json",null);
-  particlesJS.load("particles-js-nav", "assets/particlesjs-config.json",null);
-  particlesJS.load("particledrop", "assets/particlesjs-config.json",null);
+  particlesJS.load("particles-js", "assets/particlesjs-config.json", null);
+  /* particlesJS.load("particles-js-nav", "assets/particlesjs-config.json", null);
+  particlesJS.load("particledrop", "assets/particlesjs-config.json", null); */
 
 }, false);
+
+const checkboxmenu = document.querySelector('#checkbox_toggle');
+document.querySelectorAll('.menu a').forEach((menuItem) => {
+  menuItem.addEventListener('click', () => {
+    console.log('Menu item clicked'); // Add this line
+    checkboxmenu.checked = false;
+  });
+});
+
